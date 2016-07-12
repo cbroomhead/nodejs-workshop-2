52d96d5ac1c1cdb39765289674791811678ab868
Celine Broomhead <celine.broomhead@gmail.com>
2016-07-12 19:33:27 +0000

    Adding the request as JSON before I delete everything to start the How's the weather? excercise

--- request-as-json.js
+++ request-as-json.js
@@ -30,16 +30,3 @@ requestJson('https://api.wheretheiss.at/v1/satellites/25544', function (err, res
         //console.log(res);
     }
 })
-
-/*function trackISSmotherfucker(){
-request(url, function(err, position) {
-    if (err) {
-        console.log('there was an error');
-    }
-    else {
-        var lat= (JSON.parse(position.body).latitude).toFixed(2);
-        var lon= (JSON.parse(position.body).longitude).toFixed(2);
-        console.log("the latitude is " + lat + " and the longitude is " + lon);
-    }
-    });
-}*/
